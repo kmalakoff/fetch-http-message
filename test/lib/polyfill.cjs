80d6f10cf0e8a627e34e73156e7b6d7da260c28f
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-26 12:01:23 -0700

    Update formatting

--- test/lib/polyfill.cjs
+++ test/lib/polyfill.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 function polyfill() {
-  if (typeof Buffer !== "undefined" && !Buffer.alloc) {
+  if (typeof Buffer !== 'undefined' && !Buffer.alloc) {
     Buffer.alloc = function alloc(length, data) {
       let buffer = data;
       while (--length > 0) buffer += data;
