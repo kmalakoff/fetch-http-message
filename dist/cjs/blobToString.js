f26bf0b912a30a7dfcc45fff15941c36f7fd66cf
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:11:37 -0700

    Update ts-dev-stack

--- dist/cjs/blobToString.js
+++ dist/cjs/blobToString.js
@@ -1,23 +1,13 @@
 "use strict";
-
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
-exports["default"] = blobToString;
-
-// https://stackoverflow.com/a/23024613/3150390
-
-/* c8 ignore start */
+module.exports = blobToString;
 function blobToString(b) {
-  var u = URL.createObjectURL(b);
-  var x = new XMLHttpRequest();
-  x.open('GET', u, false);
-  x.send();
-  URL.revokeObjectURL(u);
-  return x.responseText;
-}
-/* c8 ignore stop */
-
-
-module.exports = exports.default;
-//# sourceMappingURL=blobToString.js.map
+    var u = URL.createObjectURL(b);
+    var x = new XMLHttpRequest();
+    x.open("GET", u, false);
+    x.send();
+    URL.revokeObjectURL(u);
+    return x.responseText;
+} /* c8 ignore stop */ 
--- dist/esm/index.mjs
+++ dist/esm/index.mjs
@@ -1,11 +1,10 @@
-import blobToString from "./blobToString.mjs";
+import blobToString from './blobToString.mjs';
 const hasHeaders = typeof Headers !== 'undefined';
 const hasBlob = typeof Blob !== 'undefined';
 const hasBuffer = typeof Buffer !== 'undefined';
-const hasURLSearchParams = typeof URLSearchParams !== 'undefined'; // export type RequestRedirect = 'error' | 'follow' | 'manual';
-
+const hasURLSearchParams = typeof URLSearchParams !== 'undefined';
 function isRequest(object) {
-  return typeof object === 'object';
+    return typeof object === 'object';
 }
 /**
  * Generate an http message string using the fetch API
@@ -13,53 +12,41 @@ function isRequest(object) {
  * @param input Fetch input
  * @param init Fetch init
  * @returns The http message string
- */
-
-
-export default function fetchHttpMessage(input, init) {
-  if (input === undefined) throw new Error('Input is expected');
-  if (init === undefined) init = {};
-  let url;
-  if (isRequest(input)) url = input.url;else {
-    url = input;
-    input = {};
-  }
-  let method = init.method || input.method || 'GET';
-  method = method.toUpperCase();
-  const lines = [`${method} ${url} HTTP/1.1`];
-  const headers = init.headers || input.headers;
-
-  if (headers !== undefined) {
-    /* c8 ignore start */
-    if (hasHeaders && headers instanceof Headers) {
-      for (const pair of headers.entries()) lines.push(`${pair[0]}: ${pair[1]}`);
-    } else {
-      /* c8 ignore stop */
-      for (const key in headers) lines.push(`${key}: ${headers[key]}`);
+ */ export default function fetchHttpMessage(input, init) {
+    if (input === undefined) throw new Error('Input is expected');
+    if (init === undefined) init = {};
+    let url;
+    if (isRequest(input)) url = input.url;
+    else {
+        url = input;
+        input = {};
     }
-  }
-
-  const body = init.body;
-
-  if (body !== undefined) {
-    if (~['GET', 'HEAD'].indexOf(method)) throw new Error(`Option body not valid with method ${method}`);
-    /* c8 ignore start */
-
-    if (hasBlob && body instanceof Blob) {
-      lines.push('');
-      lines.push(blobToString(body));
-    } else if (
-    /* c8 ignore stop */
-    typeof body === 'string' || body instanceof String ||
-    /* c8 ignore start */
-    hasBuffer && body instanceof Buffer || hasURLSearchParams && body instanceof URLSearchParams
-    /* c8 ignore stop */
-    ) {
-      lines.push('');
-      lines.push(body.toString());
-    } else throw new Error('Option body should be convertible to a string');
-  }
-
-  return lines.join('\r\n');
-}
-//# sourceMappingURL=index.mjs.map
+    let method = init.method || input.method || 'GET';
+    method = method.toUpperCase();
+    const lines = [
+        `${method} ${url} HTTP/1.1`
+    ];
+    const headers = init.headers || input.headers;
+    if (headers !== undefined) {
+        /* c8 ignore start */ if (hasHeaders && headers instanceof Headers) {
+            for (const pair of headers.entries())lines.push(`${pair[0]}: ${pair[1]}`);
+        } else {
+            /* c8 ignore stop */ for(const key in headers)lines.push(`${key}: ${headers[key]}`);
+        }
+    }
+    const body = init.body;
+    if (body !== undefined) {
+        if (~[
+            'GET',
+            'HEAD'
+        ].indexOf(method)) throw new Error(`Option body not valid with method ${method}`);
+        /* c8 ignore start */ if (hasBlob && body instanceof Blob) {
+            lines.push('');
+            lines.push(blobToString(body));
+        } else if (/* c8 ignore stop */ typeof body === 'string' || body instanceof String || /* c8 ignore start */ (hasBuffer && body instanceof Buffer) || hasURLSearchParams && body instanceof URLSearchParams) {
+            lines.push('');
+            lines.push(body.toString());
+        } else throw new Error('Option body should be convertible to a string');
+    }
+    return lines.join('\r\n');
+};
