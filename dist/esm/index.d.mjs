cbaba20e4124b1a82f1f510c64a0d28b7fba6b69
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 06:44:46 -0800

    Upgrade dependencies

---
+++ dist/esm/index.d.mjs
@@ -0,0 +1,8 @@
+/// <reference types="node" />
+/**
+ * Generate an http message string using the fetch API
+ *
+ * @param input Fetch input
+ * @param init Fetch init
+ * @returns The http message string
+ */ export { };
