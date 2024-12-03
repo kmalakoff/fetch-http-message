45ad6d5bb2ba6360256283786467b05b7aa629ba
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-03 12:28:41 -0800

    Upgrade modules

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,44 +1,45 @@
-import '../lib/polyfill.cjs';
 import assert from 'assert';
-import fetchMessage, { HeadersObject } from 'fetch-http-message';
+// @ts-ignore
+import fetchMessage, { type HeadersObject } from 'fetch-http-message';
+import '../polyfills.cjs';
 
 const url = 'https://test.com/';
 
-describe('fetch-http-message', function () {
-  it('error: no url', function () {
+describe('fetch-http-message', () => {
+  it('error: no url', () => {
     assert.throws(() => fetchMessage(undefined));
   });
 
-  it('defaults', function () {
+  it('defaults', () => {
     const message = fetchMessage(url);
     assert.equal(message, `GET ${url} HTTP/1.1`);
   });
 
-  it('default method', function () {
+  it('default method', () => {
     const message = fetchMessage(url, {});
     assert.equal(message, `GET ${url} HTTP/1.1`);
   });
 
-  describe('method', function () {
-    it('POST', function () {
+  describe('method', () => {
+    it('POST', () => {
       const message = fetchMessage(url, { method: 'POST' });
       assert.equal(message, `POST ${url} HTTP/1.1`);
     });
 
-    it('POST body', function () {
+    it('POST body', () => {
       const message = fetchMessage(url, { method: 'POST', body: 'post-body' });
       assert.equal(message, [`POST ${url} HTTP/1.1`, '', 'post-body'].join('\r\n'));
     });
   });
 
-  describe('HeadersObject', function () {
-    it('multiple headers', function () {
+  describe('HeadersObject', () => {
+    it('multiple headers', () => {
       const headers = { header1: 'value 1', header2: 'value 2' };
       const message = fetchMessage(url, { method: 'POST', headers });
       assert.equal(message, [`POST ${url} HTTP/1.1`, 'header1: value 1', 'header2: value 2'].join('\r\n'));
     });
 
-    it('multiple headers and body', function () {
+    it('multiple headers and body', () => {
       const headers = { header1: 'value 1', header2: 'value 2' };
       const message = fetchMessage(url, { method: 'PATCH', headers, body: 'post-body' });
       assert.equal(message, [`PATCH ${url} HTTP/1.1`, 'header1: value 1', 'header2: value 2', '', 'post-body'].join('\r\n'));
@@ -46,8 +47,8 @@ describe('fetch-http-message', function () {
   });
 
   typeof Headers === 'undefined' ||
-    describe('Headers', function () {
-      it('multiple headers', function () {
+    describe('Headers', () => {
+      it('multiple headers', () => {
         const headers = new Headers();
         headers.set('header1', 'value 1');
         headers.set('header2', 'value 2');
@@ -55,7 +56,7 @@ describe('fetch-http-message', function () {
         assert.equal(message, [`POST ${url} HTTP/1.1`, 'header1: value 1', 'header2: value 2'].join('\r\n'));
       });
 
-      it('multiple headers and body', function () {
+      it('multiple headers and body', () => {
         const headers = new Headers();
         headers.set('header1', 'value 1');
         headers.set('header2', 'value 2');
@@ -64,30 +65,30 @@ describe('fetch-http-message', function () {
       });
     });
 
-  describe('body', function () {
-    it('error: GET with body', function () {
+  describe('body', () => {
+    it('error: GET with body', () => {
       assert.throws(() => fetchMessage(url, { method: 'GET', body: 'get-body' }));
     });
 
-    it('error: HEAD with body', function () {
+    it('error: HEAD with body', () => {
       assert.throws(() => fetchMessage(url, { method: 'HEAD', body: 'get-body' }));
     });
 
-    it('error: non-string body', function () {
+    it('error: non-string body', () => {
       const body = function body() {
         return null;
       } as unknown;
       assert.throws(() => fetchMessage(url, { method: 'POST', body: body as string }));
     });
 
-    it('string body', function () {
+    it('string body', () => {
       const message = fetchMessage(url, { method: 'POST', body: 'post-body' });
       assert.equal(message, [`POST ${url} HTTP/1.1`, '', 'post-body'].join('\r\n'));
     });
 
     typeof Blob === 'undefined' ||
       typeof XMLHttpRequest === 'undefined' ||
-      it('Blob body', function () {
+      it('Blob body', () => {
         const message = fetchMessage(url, {
           method: 'POST',
           body: new Blob([JSON.stringify({ test: true })], { type: 'application/json' }),
@@ -96,7 +97,7 @@ describe('fetch-http-message', function () {
       });
 
     typeof Buffer === 'undefined' ||
-      it('Buffer body', function () {
+      it('Buffer body', () => {
         const message = fetchMessage(url, {
           method: 'POST',
           body: Buffer.from('aaaaa'),
@@ -105,7 +106,7 @@ describe('fetch-http-message', function () {
       });
 
     typeof URLSearchParams === 'undefined' ||
-      it('URLSearchParams body', function () {
+      it('URLSearchParams body', () => {
         const string = 'q=URLUtils.searchParams&topic=api';
         const message = fetchMessage(url, {
           method: 'POST',
@@ -115,24 +116,24 @@ describe('fetch-http-message', function () {
       });
   });
 
-  describe('init', function () {
-    it('GET not override', function () {
+  describe('init', () => {
+    it('GET not override', () => {
       const message = fetchMessage({ url, method: 'GET', headers: {} as HeadersObject }, {});
       assert.equal(message, `GET ${url} HTTP/1.1`);
     });
 
-    it('POST override', function () {
+    it('POST override', () => {
       const message = fetchMessage({ url, method: 'GET', headers: {} as HeadersObject }, { method: 'POST' });
       assert.equal(message, `POST ${url} HTTP/1.1`);
     });
 
-    it('headers not override', function () {
+    it('headers not override', () => {
       const headers1 = { x: 'value 1', y: 'value 2' } as HeadersObject;
       const message = fetchMessage({ url, method: 'GET', headers: headers1 }, { method: 'PATCH', body: 'post-body' });
       assert.equal(message, [`PATCH ${url} HTTP/1.1`, 'x: value 1', 'y: value 2', '', 'post-body'].join('\r\n'));
     });
 
-    it('headers override', function () {
+    it('headers override', () => {
       const headers1 = { x: 'value 1', y: 'value 2' } as HeadersObject;
       const headers2 = { header1: 'value 1', header2: 'value 2' } as HeadersObject;
       const message = fetchMessage({ url, method: 'GET', headers: headers1 }, { method: 'PATCH', headers: headers2, body: 'post-body' });
