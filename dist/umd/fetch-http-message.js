47a95fd3b528c19b452b1cc4e2bbdfdf588f507b
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 19:01:23 -0800

    Upgrade modules

--- dist/umd/fetch-http-message.js
+++ dist/umd/fetch-http-message.js
@@ -8,12 +8,13 @@
   /* c8 ignore start */ function blobToString(b) {
       var u = URL.createObjectURL(b);
       var x = new XMLHttpRequest();
-      x.open("GET", u, false);
+      x.open('GET', u, false);
       x.send();
       URL.revokeObjectURL(u);
       return x.responseText;
-  }
+  } /* c8 ignore stop */
 
+  // @ts-ignore
   function _instanceof(left, right) {
       if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) {
           return !!right[Symbol.hasInstance](left);
@@ -21,12 +22,16 @@
           return left instanceof right;
       }
   }
-  var hasHeaders = typeof Headers !== "undefined";
-  var hasBlob = typeof Blob !== "undefined";
-  var hasBuffer = typeof Buffer !== "undefined";
-  var hasURLSearchParams = typeof URLSearchParams !== "undefined";
+  function _type_of(obj) {
+      "@swc/helpers - typeof";
+      return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj;
+  }
+  var hasHeaders = typeof Headers !== 'undefined';
+  var hasBlob = typeof Blob !== 'undefined';
+  var hasBuffer = typeof Buffer !== 'undefined';
+  var hasURLSearchParams = typeof URLSearchParams !== 'undefined';
   function isRequest(object) {
-      return typeof object === "object";
+      return (typeof object === "undefined" ? "undefined" : _type_of(object)) === 'object';
   }
   /**
    * Generate an http message string using the fetch API
@@ -35,7 +40,7 @@
    * @param init Fetch init
    * @returns The http message string
    */ function fetchHttpMessage(input, init) {
-      if (input === undefined) throw new Error("Input is expected");
+      if (input === undefined) throw new Error('Input is expected');
       if (init === undefined) init = {};
       var url;
       if (isRequest(input)) url = input.url;
@@ -43,7 +48,7 @@
           url = input;
           input = {};
       }
-      var method = init.method || input.method || "GET";
+      var method = init.method || input.method || 'GET';
       method = method.toUpperCase();
       var lines = [
           "".concat(method, " ").concat(url, " HTTP/1.1")
@@ -78,18 +83,18 @@
       var body = init.body;
       if (body !== undefined) {
           if (~[
-              "GET",
-              "HEAD"
+              'GET',
+              'HEAD'
           ].indexOf(method)) throw new Error("Option body not valid with method ".concat(method));
           /* c8 ignore start */ if (hasBlob && _instanceof(body, Blob)) {
-              lines.push("");
+              lines.push('');
               lines.push(blobToString(body));
-          } else if (/* c8 ignore stop */ typeof body === "string" || _instanceof(body, String) || /* c8 ignore start */ (hasBuffer && _instanceof(body, Buffer)) || hasURLSearchParams && _instanceof(body, URLSearchParams)) {
-              lines.push("");
+          } else if (/* c8 ignore stop */ typeof body === 'string' || _instanceof(body, String) || /* c8 ignore start */ hasBuffer && _instanceof(body, Buffer) || hasURLSearchParams && _instanceof(body, URLSearchParams)) {
+              lines.push('');
               lines.push(body.toString());
-          } else throw new Error("Option body should be convertible to a string");
+          } else throw new Error('Option body should be convertible to a string');
       }
-      return lines.join("\r\n");
+      return lines.join('\r\n');
   }
 
   return fetchHttpMessage;
