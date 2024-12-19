086bad09c2b40f0e5ed102484648becdc19ab321
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-19 08:28:24 -0800

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-// @ts-ignore
-import blobToString from './blobToString.ts';
+import blobToString from './blobToString.js';
 
 export interface HeadersObject {
   [key: string]: string;
@@ -45,7 +44,7 @@ export default function fetchHttpMessage(input: RequestInfo, init?: RequestInit)
   if (input === undefined) throw new Error('Input is expected');
   if (init === undefined) init = {};
 
-  let url;
+  let url: string;
   if (isRequest(input)) url = (input as Request).url;
   else {
     url = input as string;
