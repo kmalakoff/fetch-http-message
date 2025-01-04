9a0822a9a47a105c34b7ae2a57c1fadb4f382f6c
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:18:18 -0800

    Upgrade modules

--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
-const fetchMessage = require('fetch-http-message/dist/umd/fetch-http-message.js');
+const fetchMessage = require('fetch-http-message/dist/umd/fetch-http-message.cjs');
 
 const url = 'https://test.com/';
 
-describe('exports fetch-http-message/dist/umd/fetch-http-message.js', function () {
-  it('defaults', function () {
+describe('exports fetch-http-message/dist/umd/fetch-http-message.cjs', () => {
+  it('defaults', () => {
     const message = fetchMessage(url);
     assert.equal(message, `GET ${url} HTTP/1.1`);
   });
--- test/lib/polyfill.cjs
+++
@@ -1,8 +0,0 @@
-function polyfill() {
-  if (typeof Buffer !== 'undefined' && !Buffer.from) {
-    Buffer.from = function from(data) {
-      return new Buffer(data);
-    };
-  }
-}
-polyfill();
