d14f9fa984b92a604ffce07ffc69aa271e0770b6
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:18:18 -0800

    0.1.10

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -1,10 +1,11 @@
 import assert from 'assert';
+// @ts-ignore
 import fetchMessage from 'fetch-http-message';
 
 const url = 'https://test.com/';
 
-describe('exports .ts', function () {
-  it('defaults', function () {
+describe('exports .ts', () => {
+  it('defaults', () => {
     const message = fetchMessage(url);
     assert.equal(message, `GET ${url} HTTP/1.1`);
   });
